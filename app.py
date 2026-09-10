6d86e70f9dbc5656171cb1045a520ac443017ffa
shaiwaltechy <shaiwaltechy@users.noreply.github.com>
2026-09-10 09:44:57 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-120b",
+    messages=[
+        {"role": "user", "content": "Say hello in one line"}
+    ],
+)
+
+print(response.choices[0].message.content)
